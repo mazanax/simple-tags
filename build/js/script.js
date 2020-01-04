9634a710bc0ac9759073f8f9e604d0e2eb408977
mazanax <mazanax@yandex.ru>
2020-01-04 15:46:03 +0300

    Implemented new version of Tags

--- build/js/script.js
+++ build/js/script.js
@@ -1,88 +1,135 @@
-function Tags(element) {
-  var DOMParent = element;
-  var DOMList;
-  var DOMInput;
-  var dataAttribute;
-  var arrayOfList;
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-  function DOMCreate() {
-    var ul = document.createElement('ul');
-    var li = document.createElement('li');
-    var input = document.createElement('input');
-    DOMParent.appendChild(ul);
-    DOMParent.appendChild(input); // first child is <ul>
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
 
-    DOMList = DOMParent.firstElementChild; // last child is <input>
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
 
-    DOMInput = DOMParent.lastElementChild;
+var Tags =
+/*#__PURE__*/
+function () {
+  function Tags(element, options) {
+    _classCallCheck(this, Tags);
+
+    options = options || {};
+    this.DOMParent = element;
+    this.data = options.data || [];
+    this.preventSubmitting = options.preventSubmitting || false;
+    this.inputClass = options.inputClass || null;
+    this.deleteWithoutConfirmation = options.deleteWithoutConfirmation || false;
+
+    this.onTagAdded = options.onTagAdded || function () {};
+
+    this.onTagDeleted = options.onTagDeleted || function () {};
+
+    this.getAttribute();
+    this.DOMCreate();
+    this.DOMRender();
+    this.onKeyUp();
   }
 
-  function DOMRender() {
-    // clear the entire <li> inside <ul> 
-    DOMList.innerHTML = ''; // render each <li> to <ul>
+  _createClass(Tags, [{
+    key: "DOMCreate",
+    value: function DOMCreate() {
+      var ul = document.createElement('ul');
+      var input = document.createElement('input');
+      this.DOMParent.appendChild(ul);
+      this.DOMParent.appendChild(input); // first child is <ul>
 
-    arrayOfList.forEach(function (currentValue, index) {
-      var li = document.createElement('li');
-      li.innerHTML = "".concat(currentValue, " <a>&times;</a>");
-      li.querySelector('a').addEventListener('click', function () {
-        if (confirm('Continue to remove tag?')) {
-          onDelete(index);
+      this.DOMList = this.DOMParent.firstElementChild; // last child is <input>
+
+      this.DOMInput = this.DOMParent.lastElementChild;
+    }
+  }, {
+    key: "DOMRender",
+    value: function DOMRender() {
+      var that = this;
+      this.DOMList.innerHTML = ''; // render each <li> to <ul>
+
+      this.arrayOfList.forEach(function (currentValue, index) {
+        var li = document.createElement('li');
+        li.innerHTML = "".concat(currentValue, " <a>&times;</a>");
+        li.querySelector('a').addEventListener('click', function () {
+          that.onDelete(index);
+          return false;
+        });
+        that.DOMList.appendChild(li);
+        that.setAttribute();
+      });
+    }
+  }, {
+    key: "onKeyUp",
+    value: function onKeyUp() {
+      var that = this;
+      var previous = '';
+      this.DOMInput.addEventListener('keyup', function (event) {
+        var text = this.value.trim(); // check if ',' or 'enter' key was press
+
+        if (event.keyCode === 13 && that.preventSubmitting) {
+          event.preventDefault();
         }
 
-        return false;
+        if (event.keyCode === 8 && that.arrayOfList && !previous && !text) {
+          that.onDelete(that.arrayOfList.length - 1);
+        }
+
+        if (text.includes(',') || event.keyCode === 13) {
+          // check if empty text when ',' is remove
+          var addedTag = text.replace(',', '');
+
+          if (addedTag !== '') {
+            that.arrayOfList.push(addedTag);
+            that.onTagAdded(addedTag);
+          } // clear input
+
+
+          this.value = '';
+        }
+
+        that.DOMRender();
+        previous = this.value.trim();
       });
-      DOMList.appendChild(li);
-      setAttribute();
-    });
-  }
+    }
+  }, {
+    key: "onDelete",
+    value: function onDelete(id) {
+      var that = this;
 
-  function onKeyUp() {
-    DOMInput.addEventListener('keyup', function (event) {
-      var text = this.value.trim(); // check if ',' or 'enter' key was press
-
-      if (text.includes(',') || event.keyCode == 13) {
-        // check if empty text when ',' is remove
-        if (text.replace(',', '') != '') {
-          // push to array and remove ','
-          arrayOfList.push(text.replace(',', ''));
-        } // clear input
-
-
-        this.value = '';
+      if (that.deleteWithoutConfirmation || !confirm("Continue to remove tag ".concat(this.arrayOfList[id], "?"))) {
+        return;
       }
 
-      DOMRender();
-    });
-  }
+      this.arrayOfList = this.arrayOfList.filter(function (currentValue, index) {
+        if (index === id) {
+          that.onTagDeleted(currentValue);
+          return false;
+        }
 
-  function onDelete(id) {
-    arrayOfList = arrayOfList.filter(function (currentValue, index) {
-      if (index == id) {
-        return false;
+        return currentValue;
+      });
+      this.DOMRender();
+    }
+  }, {
+    key: "getAttribute",
+    value: function getAttribute() {
+      var data = this.data || [],
+          dataAttributes = '';
+
+      if (!data && (dataAttributes = this.DOMParent.getAttribute('data-simple-tags'))) {
+        data = dataAttributes.split(','); // store array of data attribute in arrayOfList
       }
 
-      return currentValue;
-    });
-    DOMRender();
-  }
+      this.arrayOfList = data.map(function (currentValue) {
+        return currentValue.trim();
+      });
+    }
+  }, {
+    key: "setAttribute",
+    value: function setAttribute() {
+      this.DOMParent.setAttribute('data-simple-tags', this.arrayOfList.toString());
+    }
+  }]);
 
-  function getAttribute() {
-    dataAttribute = DOMParent.getAttribute('data-simple-tags');
-    dataAttribute = dataAttribute.split(','); // store array of data attribute in arrayOfList
-
-    arrayOfList = dataAttribute.map(function (currentValue) {
-      return currentValue.trim();
-    });
-  }
-
-  function setAttribute() {
-    DOMParent.setAttribute('data-simple-tags', arrayOfList.toString());
-  }
-
-  getAttribute();
-  DOMCreate();
-  DOMRender();
-  onKeyUp();
-}
+  return Tags;
+}();
 
 export default Tags;
--- source/script/script.js
+++ source/script/script.js
@@ -1,95 +1,115 @@
-function Tags( element ) {
-	let DOMParent = element
-	let DOMList
-	let DOMInput
-	let dataAttribute
-	let arrayOfList
-	
-	function DOMCreate() {
-		let ul 		= document.createElement('ul')
-		let li 		= document.createElement('li')
-		let input 	= document.createElement('input')
-		
-		DOMParent.appendChild( ul )
-		DOMParent.appendChild( input )
-		
-		// first child is <ul>
-		DOMList 	= DOMParent.firstElementChild
-		// last child is <input>
-		DOMInput	= DOMParent.lastElementChild
-	}
-	
-	function DOMRender() {
-		// clear the entire <li> inside <ul> 
-		DOMList.innerHTML = ''
-		
-		// render each <li> to <ul>
-		arrayOfList.forEach(( currentValue, index ) => {
-			let li 				= document.createElement('li')
-				li.innerHTML 	= `${currentValue} <a>&times;</a>`
-				li.querySelector('a').addEventListener( 'click', function() {
-					if( confirm('Continue to remove tag?') ){
-						onDelete( index )	 
-					}
+class Tags {
+    constructor(element, options) {
+        options = options || {};
 
-					return false;
-				})
-			
-			DOMList.appendChild( li )
-			setAttribute()
-		})
-		
-		
-	}
-	
-	function onKeyUp() {
-		DOMInput.addEventListener( 'keyup', function( event ) {
-			let text = this.value.trim()
-			
-			// check if ',' or 'enter' key was press
-			if ( text.includes(',') || event.keyCode == 13 ) {
-				// check if empty text when ',' is remove
-				if ( text.replace(',', '') != '') {
-					// push to array and remove ','
-					arrayOfList.push( text.replace(',' , '') )
-				}
-				// clear input
-				this.value = ''
-			}
-			
-			DOMRender()
-		})
-	}
-	
-	function onDelete( id ) {
-		arrayOfList = arrayOfList.filter(function( currentValue, index ) {
-			if (index == id ) {
-				return false
-			}
-			return currentValue
-		})
-		
-		DOMRender()
-	}
-	
-	function getAttribute() {
-		dataAttribute = DOMParent.getAttribute('data-simple-tags')
-		dataAttribute = dataAttribute.split(',')
-		
-		// store array of data attribute in arrayOfList
-		arrayOfList = dataAttribute.map(( currentValue ) => {
-			return currentValue.trim()
-		})
-	}
-	
-	function setAttribute() {
-		DOMParent.setAttribute('data-simple-tags', arrayOfList.toString());
-	}
-	
-	getAttribute()
-	DOMCreate()
-	DOMRender()
-	onKeyUp()
+        this.DOMParent = element;
+        this.data = options.data || []
+        this.preventSubmitting = options.preventSubmitting || false;
+        this.inputClass = options.inputClass || null;
+        this.deleteWithoutConfirmation = options.deleteWithoutConfirmation || false;
+        this.onTagAdded = options.onTagAdded || function () {};
+        this.onTagDeleted = options.onTagDeleted || function () {};
+
+        this.getAttribute();
+        this.DOMCreate();
+        this.DOMRender();
+        this.onKeyUp();
+    }
+
+    DOMCreate() {
+        let ul = document.createElement('ul');
+        let input = document.createElement('input');
+
+        this.DOMParent.appendChild(ul);
+        this.DOMParent.appendChild(input); // first child is <ul>
+
+        this.DOMList = this.DOMParent.firstElementChild; // last child is <input>
+        this.DOMInput = this.DOMParent.lastElementChild;
+    }
+
+    DOMRender() {
+        const that = this;
+        this.DOMList.innerHTML = ''; // render each <li> to <ul>
+
+        this.arrayOfList.forEach(function (currentValue, index) {
+            let li = document.createElement('li');
+            li.innerHTML = "".concat(currentValue, " <a>&times;</a>");
+            li.querySelector('a').addEventListener('click', function () {
+                that.onDelete(index);
+
+                return false;
+            });
+            that.DOMList.appendChild(li);
+            that.setAttribute();
+        });
+    }
+
+    onKeyUp() {
+        const that = this;
+        let previous = '';
+
+        this.DOMInput.addEventListener('keyup', function (event) {
+            let text = this.value.trim(); // check if ',' or 'enter' key was press
+
+            if (event.keyCode === 13 && that.preventSubmitting) {
+                event.preventDefault();
+            }
+
+            if (event.keyCode === 8 && that.arrayOfList && !previous && !text) {
+                that.onDelete(that.arrayOfList.length - 1);
+            }
+
+            if (text.includes(',') || event.keyCode === 13) {
+                // check if empty text when ',' is remove
+                let addedTag = text.replace(',', '');
+                if (addedTag !== '') {
+                    that.arrayOfList.push(addedTag);
+                    that.onTagAdded(addedTag)
+                } // clear input
+
+                this.value = '';
+            }
+
+            that.DOMRender();
+            previous = this.value.trim();
+        });
+    }
+
+    onDelete(id) {
+        let that = this;
+
+        if (that.deleteWithoutConfirmation || !confirm(`Continue to remove tag ${this.arrayOfList[id]}?`)) {
+            return;
+        }
+
+        this.arrayOfList = this.arrayOfList.filter(function (currentValue, index) {
+            if (index === id) {
+                that.onTagDeleted(currentValue);
+
+                return false;
+            }
+
+            return currentValue;
+        });
+        this.DOMRender();
+    }
+
+    getAttribute() {
+        let data = this.data || [],
+            dataAttributes = '';
+
+        if (!data && (dataAttributes = this.DOMParent.getAttribute('data-simple-tags'))) {
+            data = dataAttributes.split(','); // store array of data attribute in arrayOfList
+        }
+
+        this.arrayOfList = data.map(function (currentValue) {
+            return currentValue.trim();
+        });
+    }
+
+    setAttribute() {
+        this.DOMParent.setAttribute('data-simple-tags', this.arrayOfList.toString());
+    }
 }
 
 export default Tags;
